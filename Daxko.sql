9e2ffaf80daaed1e96c693c188d548200465126d
carolinelarry <79863465+carolinelarry@users.noreply.github.com>
2021-10-10 21:36:59 -0500

    Adding new changes

--- Daxko.sql
+++ Daxko.sql
@@ -1,89 +1,165 @@
 -- 1) Provide all addresses that are located in Texas
 
-Select * from Person.StateProvince
-where Name = 'Texas' 
+SELECT * FROM Person.Address;
 
-select * from Person.Address
-where StateProvinceID = 73
+SELECT * FROM  Person.StateProvince;
 
-SELECT Person.Address.AddressLine1, Person.Address.AddressLine2, Person.Address.City, Person.StateProvince.Name AS "State", Person.Address.PostalCode
-from Person.Address
-INNER JOIN Person.StateProvince ON
-Person.Address.StateProvinceID = Person.StateProvince.StateProvinceID
-WHERE Person.Address.StateProvinceID = 73;
+SELECT 
+    Person.Address.AddressLine1, 
+    Person.Address.AddressLine2, 
+    Person.Address.City, 
+    Person.StateProvince.Name AS "State", 
+    Person.Address.PostalCode
+FROM Person.Address
+INNER JOIN Person.StateProvince
+    ON Person.Address.StateProvinceID = Person.StateProvince.StateProvinceID
+WHERE Person.Address.StateProvinceID IN (
+    SELECT StateProvinceID 
+    FROM Person.StateProvince
+    WHERE Name = 'Texas' 
+);  
+
 
 -- 2) All products that have a standard cost greater than $50 and a list price more than double the standard cost 
 
 SELECT * FROM Production.Product
-WHERE StandardCost > 50 AND 
-ListPrice > (2 * StandardCost);
+WHERE StandardCost > 50 AND ListPrice > (2 * StandardCost);
 
 -- 3) Update "St." to "Street"
 
 SELECT * FROM Person.Address;
 
 UPDATE Person.Address  
-SET  AddressLine1 = REPLACE(AddressLine1, 'St.', 'Street')
-SELECT @@ROWCOUNT;
+SET  AddressLine1 = REPLACE(AddressLine1, 'St.', 'Street');
 
 SELECT * FROM Person.Address;
 
 -- 4) Up list price $25
+
+SELECT * FROM Production.Product;
 
 -- First update list price in Product table
 UPDATE Production.Product
 SET ListPrice = ListPrice + 25
 WHERE Name = 'All-Purpose Bike Stand';
 
-SELECT * FROM Production.Product
-WHERE Name = 'All-Purpose Bike Stand';
 
 --Insert record into list price history table and update end date in previous entry
 SELECT * FROM Production.ProductListPriceHistory;
 
-SELECT * from Production.Product
-WHERE Name = 'All-Purpose Bike Stand';
---Product ID is 879
-
-SELECT * FROM Production.ProductListPriceHistory
-WHERE ProductID = 879;
-
 UPDATE Production.ProductListPriceHistory
-SET EndDate = '2021-10-12 00:00:00.000'
-WHERE ProductID = 879 AND ListPrice = 159;
+SET EndDate = DATEADD(day, 0, getdate())
+WHERE ProductID IN (
+    SELECT ProductID FROM Production.Product
+    WHERE Name = 'All-Purpose Bike Stand'
+);
+
+SELECT ProductID FROM Production.Product
+WHERE Name = 'All-Purpose Bike Stand';
+--879
 
 INSERT INTO Production.ProductListPriceHistory (ProductID, StartDate, EndDate, ListPrice, ModifiedDate)
-VALUES (879, '2021-10-12 00:00:00.000', NULL, 184.0000, '2021-10-12 00:00:00.000');
+VALUES (879, DATEADD(day, 0, getdate()), NULL, 184.0000, DATEADD(day, 0, getdate()));
+
+--DELETE FROM Production.ProductListPriceHistory WHERE ProductID = '879' AND ListPrice = 184;
 
 SELECT * FROM Production.ProductListPriceHistory
 WHERE ProductID = 879;
 
---MESS AROUND WITH THIS LATER
---Convert(DateTime,'19820626',112)
 
 -- 5) Human Rources Pay History Table
 
 SELECT * FROM HumanResources.EmployeePayHistory;
 
+--Creating view--
+CREATE VIEW RateChangeView AS
+SELECT
+	  RateChangeDate,
+	  DATEDIFF(DAY, LAG(RateChangeDate) OVER (ORDER BY RateChangeDate), RateChangeDate)
+                AS DaysSinceLastRateChange,
+    Count(BusinessEntityID) OVER (Partition BY RateChangeDate) AS NumberofRateChanges,
+    Rate
+FROM  HumanResources.EmployeePayHistory;
+
+
+SELECT * FROM RateChangeView;
+
+--------------------------------------------------------------
+SELECT *
+    --LAG(RateChangeDate) OVER (ORDER BY RateChangeDate) AS PreviousDate
+FROM RateChangeView
+WHERE DaysSinceLastRateChange = 
+    (SELECT MAX(DaysSinceLastRateChange)
+    FROM RateChangeView);
+
+--------------------------------------------------------------
+--Query ordering results by DaysSinceLastRateChange
+SELECT
+	  RateChangeDate,
+      ModifiedDate,
+	  DATEDIFF(DAY, LAG(RateChangeDate) OVER (ORDER BY RateChangeDate), RateChangeDate) AS DaysSinceLastRateChange,
+    Count(BusinessEntityID) OVER (Partition BY RateChangeDate)AS NumberofRateChanges,
+    Rate
+FROM  HumanResources.EmployeePayHistory;
+--ORDER BY DaysSinceLastRateChange DESC;
+
 -- 6) 
 
-SELECT * FROM Person.Person
-WHERE LastName = 'Blythe';
---BusinessEntityID either 275 or 495
+SELECT * FROM Sales.SalesOrderHeader;
 
-SELECT * FROM Sales.SalesOrderHeader
-WHERE SalesPersonID = '274' or SalesPersonID = '495';
---Id = 274
+--Created view to use later
+CREATE VIEW SalesReport5 AS
+    SELECT 
+        year(OrderDate) AS Year, 
+        SalesPersonID, 
+        TaxAmt, 
+        SubTotal, 
+        (TaxAmt + SubTotal) AS TotalSales,
+        SUM(TaxAmt + SubTotal) OVER (PARTITION BY SalesPersonID, year(OrderDate) ORDER BY year(OrderDate)) AS SalesPersonTotal,
+        SUM(TaxAmt + SubTotal) OVER (PARTITION BY year(OrderDate) ORDER BY year(OrderDate)) AS YearlyTotal
+    FROM Sales.SalesOrderHeader;
 
---Is freight included in tax amount? I don't think it is because you have to add all 3 up to get "total"
--- select sum(*) as qty, datepart(yyyy, [date]) as [year]
--- from shoptransfer 
--- group by datepart(yyyy, [date])
--- order by [year]
+SELECT * FROM SalesReport5;
 
-SELECT *, (SubTotal + TaxAmt) as 'Total Sales'
-from Sales.SalesOrderHeader;
---group above query by year/ sum it up for each year
+--Year, Total Sales, and Percentage of Total Sales for Michael Blythe
+SELECT
+    Year,
+    AVG(YearlyTotal) as TotalSales,
+    AVG((SalesPersonTotal / YearlyTotal) * 100) AS PercentSales
+FROM SalesReport5
+WHERE SalesPersonID IN (
+    SELECT BusinessEntityID FROM Person.Person
+    WHERE LastName = 'Blythe'
+)
+GROUP BY Year
+ORDER BY Year
+
+
+-- 7) Provide listing for every employee hired after Jan 1, 2008 who has had their pay rate changed
+
+SELECT * FROM HumanResources.Employee --hire date, jobtitle, 
+SELECT * FROM Person.Person --first name, last name, 
+SELECT * FROM HumanResources.EmployeePayHistory --pay history
+
+
+SELECT Person.Person.BusinessEntityID, Person.Person.FirstName, Person.Person.LastName, 
+    HumanResources.Employee.JobTitle, HumanResources.Employee.HireDate, HumanResources.EmployeePayHistory.RateChangeDate,
+    HumanResources.EmployeePayHistory.Rate
+FROM Person.Person
+--Joining three tables on BusinessEntityID
+INNER JOIN HumanResources.Employee ON
+    Person.Person.BusinessEntityID = HumanResources.Employee.BusinessEntityID
+INNER JOIN HumanResources.EmployeePayHistory ON
+    HumanResources.Employee.BusinessEntityID = HumanResources.EmployeePayHistory.BusinessEntityID
+--Only including employees hired after Jan 1, 2008 and who have had their pay rate changed since hire date
+WHERE HireDate > '01-08-2008' AND HireDate != RateChangeDate
+ORDER BY Person.Person.BusinessEntityID
+
+
+ 
+
+
+
 
 
 
